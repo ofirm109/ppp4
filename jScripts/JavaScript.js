367c5734c762ee478963cd64ff2fc341547d023c
ofirm109 <ofirm109@walla.com>
2019-07-25 17:15:43 +0300

    Add files via upload

---
+++ jScripts/JavaScript.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    $("#ice").click(function () {
+        $('#exampleModal').modal('show');
+
+    });
+    $("#danone").click(function () {
+        
+        $('#exampleModal1').modal('show');
+    });
+
+    $('#tic').click(function () {
+        $('#exampleModal2').modal('show');
+
+    });
+    $('#pop').click(function () {
+        $('#exampleModal3').modal('show');
+
+    });
+
+});
